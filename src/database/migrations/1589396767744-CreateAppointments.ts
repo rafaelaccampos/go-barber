7028950d90fca331557968113756680398d46448
rafaelaccampos <rafaela-cassiano@hotmail.com>
2020-05-13 21:38:34 -0300

    ref: Refactor feature appointments to work with database persistency instead of local mememory

--- src/database/migrations/1589396767744-CreateAppointments.ts
+++ src/database/migrations/1589396767744-CreateAppointments.ts
@@ -11,6 +11,7 @@ export class CreateAppointments1589396767744 implements MigrationInterface {
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
