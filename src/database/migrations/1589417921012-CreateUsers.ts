a62da0102aaa81e2bba075588fefd2325af11ec7
rafaelaccampos <rafaela-cassiano@hotmail.com>
2020-05-14 05:41:59 -0300

    feat: Add users model and migration

--- src/database/migrations/1589417921012-CreateUsers.ts
+++ src/database/migrations/1589417921012-CreateUsers.ts
@@ -1,10 +1,10 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export class CreateAppointments1589396767744 implements MigrationInterface {
+export class CreateUsers1589417921012 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
-        name: 'appointments',
+        name: 'users',
         columns: [
           {
             name: 'id',
@@ -14,13 +14,19 @@ export class CreateAppointments1589396767744 implements MigrationInterface {
             default: 'uuid_generate_v4()',
           },
           {
-            name: 'provider',
+            name: 'name',
             type: 'varchar',
             isNullable: false,
           },
           {
-            name: 'date',
-            type: 'timestamp with time zone',
+            name: 'email',
+            type: 'varchar',
+            isUnique: true,
+            isNullable: false,
+          },
+          {
+            name: 'password',
+            type: 'varchar',
             isNullable: false,
           },
           {
@@ -40,6 +46,6 @@ export class CreateAppointments1589396767744 implements MigrationInterface {
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropTable('appointments');
+    await queryRunner.dropTable('users');
   }
 }
