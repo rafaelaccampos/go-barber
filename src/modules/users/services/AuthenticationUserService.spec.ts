54b9379a12f828224e128870232d14a0843da938
rafaelaccampos <rafaela-cassiano@hotmail.com>
2020-08-02 16:53:04 -0300

    ref: Removing direct dependency of Bcrypt in the users services

--- src/modules/users/services/AuthenticationUserService.spec.ts
+++ src/modules/users/services/AuthenticationUserService.spec.ts
@@ -1,17 +1,25 @@
 import AppError from '@shared/errors/AppError';
 
 import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
 import AuthenticationUserService from './AuthenticationUserService';
 import CreateUserService from './CreateUserService';
 
 describe('AuthenticateUser', () => {
   it('should be able to authenticate', async () => {
     const fakeUsersRepository = new FakeUsersRepository();
+    const fakeHashProvider = new FakeHashProvider();
 
-    const createUser = new CreateUserService(fakeUsersRepository);
-    const authenticateUser = new AuthenticationUserService(fakeUsersRepository);
+    const createUser = new CreateUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
+    const authenticateUser = new AuthenticationUserService(
+      fakeUsersRepository,
+      fakeHashProvider,
+    );
 
-    await createUser.execute({
+    const user = await createUser.execute({
       name: 'John Doe',
       email: 'johndoe@gmail.com',
       password: '123456',
@@ -23,5 +31,6 @@ describe('AuthenticateUser', () => {
     });
 
     expect(response).toHaveProperty('token');
+    expect(response.user).toEqual(user);
   });
 });
