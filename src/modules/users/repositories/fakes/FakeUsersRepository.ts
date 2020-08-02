d567b0ea3459dc81673d8e5e9e0192cf4d235472
rafaelaccampos <rafaela-cassiano@hotmail.com>
2020-08-02 12:42:05 -0300

    test: Add test to create new user

---
+++ src/modules/users/repositories/fakes/FakeUsersRepository.ts
@@ -0,0 +1,45 @@
+import User from '../../infra/typeorm/entities/User';
+import { uuid } from 'uuidv4';
+
+import IUsersRepository from '../IUsersRepository';
+import ICreateUserDTO from '@modules/users/dtos/ICreateUserDTO';
+
+class FakeUsersRepository implements IUsersRepository {
+  private users: User[] = [];
+
+  public async findById(id: string): Promise<User | undefined> {
+    const user = this.users.find(user => user.id === id);
+
+    return user;
+  }
+
+  public async findByEmail(email: string): Promise<User | undefined> {
+    const user = this.users.find(user => user.email === email);
+
+    return user;
+  }
+
+  public async create({
+    name,
+    email,
+    password,
+  }: ICreateUserDTO): Promise<User> {
+    const user = new User();
+
+    Object.assign(user, { id: uuid(), name, email, password });
+
+    this.users.push(user);
+
+    return user;
+  }
+
+  public async save(user: User): Promise<User> {
+    const indexUser = this.users.findIndex(findUser => findUser.id === user.id);
+
+    this.users[indexUser] = user;
+
+    return user;
+  }
+}
+
+export default FakeUsersRepository;
