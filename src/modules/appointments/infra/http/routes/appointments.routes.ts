2eb6c4c32058bc29ebf95e6e29438f3b7ef66dde
rafaelaccampos <rafaela-cassiano@hotmail.com>
2021-01-16 19:59:22 -0300

    feat: Create test for the feature create appointment

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,4 +1,4 @@
-import { Router } from 'express';
+import Router from 'express';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import AppointmentsController from '../controllers/AppointmentsController';
