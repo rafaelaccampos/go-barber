3a70e18ec4303982c4bf9af9de80924e3defdefe
rafaelaccampos <rafaela-cassiano@hotmail.com>
2021-01-16 22:40:46 -0300

    feat: Create validation to prevent appointments created with the same user as provider

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -20,12 +20,12 @@ describe('CreateAppointment', () => {
     })
     const appointment = await createAppointment.execute({
       date: new Date(2021, 0, 17, 24),
-      user_id: '123123',
-      provider_id: '123',
+        user_id: 'user_id',
+        provider_id: 'provider_id',
     });
 
     expect(appointment).toHaveProperty('id');
-    expect(appointment.provider_id).toBe('123');
+    expect(appointment.provider_id).toBe('provider_id');
   });
 
   it('should not be able to create two appointments on the same time', async () => {
@@ -33,15 +33,15 @@ describe('CreateAppointment', () => {
 
     const appointment = await createAppointment.execute({
       date: appointmentDate,
-      user_id: '123123',
-      provider_id: '123',
+      user_id: 'user_id',
+      provider_id: 'provider_id',
     });
 
     await expect(
       createAppointment.execute({
         date: appointmentDate,
-        user_id: '123123',
-        provider_id: '123',
+        user_id: 'user_id',
+        provider_id: 'provider_id',
       }),
     ).rejects.toBeInstanceOf(AppError);
   });
@@ -54,6 +54,20 @@ describe('CreateAppointment', () => {
     await expect(
       createAppointment.execute({
         date: new Date(2020, 4, 10, 22),
+        user_id: 'user_id',
+        provider_id: 'provider_id',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  it('should not be able to create an appointment with same user as a provider', async () => {
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      return new Date(2021, 0, 10, 12).getTime();
+    });
+
+    await expect(
+      createAppointment.execute({
+        date: new Date(2021, 0, 10, 13),
         user_id: '123123',
         provider_id: '123123'
       }),
