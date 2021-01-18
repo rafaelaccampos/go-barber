5cdae43eaf6e5e05c7ce2b8401d86c446c64ddbf
rafaelaccampos <rafaela-cassiano@hotmail.com>
2021-01-18 14:33:04 -0300

    feat: Using class-transformer library to expose avatar url and exclude of user sensitive information

--- src/modules/users/infra/http/controllers/SessionsController.ts
+++ src/modules/users/infra/http/controllers/SessionsController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
-
 import { container } from 'tsyringe';
+import { classToClass } from 'class-transformer';
 
 import AuthenticationUserService from '@modules/users/services/AuthenticationUserService';
 
@@ -15,9 +15,7 @@ class SessionsController {
       password,
     });
 
-    delete user.password;
-
-    return response.json({ user, token });
+    return response.json({ user: classToClass(user), token });
   }
 }
 
