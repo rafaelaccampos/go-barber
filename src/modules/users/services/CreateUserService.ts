869236ed1be0e58e8b559d1989ae5a1915d0230e
rafaelaccampos <rafaela-cassiano@hotmail.com>
2021-01-23 20:34:23 -0300

    feat: Done cache invalidation when a new user is created

--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -2,6 +2,7 @@ import { injectable, inject } from 'tsyringe';
 
 import AppError from '@shared/errors/AppError';
 import IUsersRepository from '../repositories/IUsersRepository';
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider';
 import IHashProvider from '../providers/HashProvider/models/IHashProvider';
 
 import User from '@modules/users/infra/typeorm/entities/User';
@@ -20,6 +21,9 @@ class CreateUserService {
 
     @inject('HashProvider')
     private hashProvider: IHashProvider,
+
+    @inject('CacheProvider')
+    private cacheProvider: ICacheProvider,
   ) {}
 
   public async execute({ name, email, password }: IRequest): Promise<User> {
@@ -37,7 +41,7 @@ class CreateUserService {
       password: hashedPassword,
     });
 
-    await this.usersRepository.save(user);
+    await this.cacheProvider.invalidatePrefix('providers-list');
 
     return user;
   }
