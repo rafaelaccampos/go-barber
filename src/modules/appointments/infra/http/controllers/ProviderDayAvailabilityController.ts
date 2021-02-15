2bb52e0d9f0695ce1f987df722372b89fe7e3309
rafaelaccampos <rafaela-cassiano@hotmail.com>
2021-02-15 06:48:25 -0300

    fix: Changing index routes to use query params instead of body

--- src/modules/appointments/infra/http/controllers/ProviderDayAvailabilityController.ts
+++ src/modules/appointments/infra/http/controllers/ProviderDayAvailabilityController.ts
@@ -6,7 +6,7 @@ import ListDayProviderAvailabilityService from '@modules/appointments/services/L
 export default class ProviderDayAvailabilityController {
   public async index(request: Request, response: Response) : Promise<Response>{
     const { provider_id } = request.params;
-    const { day, month, year } = request.body;
+    const { day, month, year } = request.query;
 
     const listDayProviderAvailabilityService = container.resolve(
       ListDayProviderAvailabilityService,
@@ -14,9 +14,9 @@ export default class ProviderDayAvailabilityController {
 
     const availability = await listDayProviderAvailabilityService.execute({
       provider_id,
-      day,
-      month,
-      year,
+      day: Number(day),
+      month: Number(month),
+      year: Number(year),
     });
 
     return response.json(availability);
